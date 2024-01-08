d84c379c7a80975194950f20d9a1f4909887d4c1
AmitGujar <amithero3342@gmail.com>
2024-01-08 16:05:39 +0530

    initial commit

---
+++ run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# ! warning - make sure you have created storage account with custom file share & secret for the same.
+
+upgrade_ingress() {
+    helm upgrade ingress-nginx ingress-nginx \
+      --repo https://kubernetes.github.io/ingress-nginx \
+      --namespace public-ingress \
+      --set controller.service.externalTrafficPolicy=Local \
+      --set tcp.9200="elastic-deployment/es-amundsen-service:9200" \
+      --set tcp.9300="elastic-deployment/es-amundsen-service:9300"
+}
+upgrade_ingress
+
+deploy() {
+    echo "deploying elastic search...."
+    kubectl apply -f deployment.yml
+
+    sleep 5
+
+    echo "update the es.akstest.tech dns record to this ip..."
+    kubectl get svc -n public-ingress
+
+    sleep 20
+
+    echo "deploying ingress....."
+    kubectl apply -f ingress.yml
+}
+deploy
